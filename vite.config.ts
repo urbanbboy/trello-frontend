f9763b5d0ce488de81d9ad39034b895974b2d6ed
talant <talant.04asanov@gmail.com>
2024-12-18 11:38:27 +0600

    fix: api url

--- vite.config.ts
+++ vite.config.ts
@@ -16,7 +16,7 @@ export default defineConfig({
     },
     define: {
         __LOCAL_BASE_URL__: JSON.stringify("http://localhost:8080/api"),
-        __RENDER_BASE_URL__: JSON.stringify("https://trello-backend-sglq.onrender.com/api"),
+        __RENDER_BASE_URL__: JSON.stringify("https://trello-backend-1-kb2l.onrender.com/api"),
         __UNSPLASH_API_KEY__: JSON.stringify('-OycfeGP9NPRcI2ln69mFnJLfj2-1BExAgarFqFJcQI'),
         _MAX_FILE_SIZE_: JSON.stringify(2000000)
     }
