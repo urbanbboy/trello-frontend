f060aef39d5c219bf13f703e569fe652c389696a
talant <talant.04asanov@gmail.com>
2025-01-15 01:03:06 +0600

    feat: task assignee

--- src/features/editTask/ui/Actions.tsx
+++ src/features/editTask/ui/Actions.tsx
@@ -1,13 +1,15 @@
-import { useDeleteTaskMutation } from "@/entities/Task/model/api";
-import { Task } from "@/entities/Task/model/types"
-import { Button } from "@/shared/ui/Button";
+import { useState } from "react";
+import { useParams } from "react-router-dom"
 import { FetchBaseQueryError } from "@reduxjs/toolkit/query";
 import { Trash } from "lucide-react";
 import { toast } from "sonner";
-import { TaskActionsResponseError } from "../model/types";
+import { useDeleteTaskMutation, useUpdateTaskMutation } from "@/entities/Task/model/api";
+import { Task } from "@/entities/Task/model/types"
+import { Button } from "@/shared/ui/Button";
 import { ButtonLoader } from "@/shared/ui/ButtonLoader";
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/shared/ui/select";
-import { useState } from "react";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/shared/ui/Select";
+import { TaskActionsResponseError } from "../model/types";
+import { useGetBoardMembersQuery } from "@/entities/Board/model/api";
 
 interface Props {
     data: Task;
@@ -15,8 +17,13 @@ interface Props {
 }
 
 export const Actions = ({ data, onClose }: Props) => {
+    const { id: boardId } = useParams<{ id: string }>()
+    const [memberId, setMemberId] = useState<string>(data.assignee || "")
     const [deleteTask, { isLoading: isDeleteLoading }] = useDeleteTaskMutation()
-    const [value, setValue] = useState<string>('')
+    const { data: boardMembers } = useGetBoardMembersQuery({ boardId })
+    const [updateTask] = useUpdateTaskMutation()
+
+    const currentAssignee = boardMembers?.find((member) => member.id === memberId)
 
     const onDelete = async () => {
         await deleteTask(data._id)
@@ -24,6 +31,24 @@ export const Actions = ({ data, onClose }: Props) => {
             .then(() => {
                 toast.success(`Задача удалена`)
                 onClose()
+            })
+            .catch((error: FetchBaseQueryError) => {
+                const data = error.data as TaskActionsResponseError;
+                toast.error(data.message)
+            })
+    }
+
+    const onChangeAssignee = async (value: string) => {
+        setMemberId(value);
+        await updateTask({
+            data: {
+                assignee: value
+            },
+            id: data._id
+        })
+            .unwrap()
+            .then((data: Task) => {
+                toast.success(`Задача ${data.title} обновлена`)
             })
             .catch((error: FetchBaseQueryError) => {
                 const data = error.data as TaskActionsResponseError;
@@ -45,18 +70,22 @@ export const Actions = ({ data, onClose }: Props) => {
                     {isDeleteLoading ? <ButtonLoader text="Удаление" /> : <span className="flex items-center gap-x-1"> <Trash /> Удалить</span>}
                 </Button>
                 <Select
-                    value={value ? `${value}` : undefined}
-                    onValueChange={(value) => {
-                        setValue(value);
-                    }}
+                    // defaultValue={data}
+                    value={memberId ? `${memberId}` : undefined}
+                    onValueChange={onChangeAssignee}
                 >
-                    <SelectTrigger className="">
-                        <SelectValue placeholder="Назначить" />
+                    <SelectTrigger className="text-black dark:text-white">
+                        <SelectValue placeholder="Назначить">
+                            {currentAssignee?.username || "Назначить"}
+                        </SelectValue>
                     </SelectTrigger>
                     <SelectContent>
-                        <SelectItem value={"1"}>
-                            user1
-                        </SelectItem>
+                        {boardMembers?.length === 0 && <>Нет участников</>}
+                        {boardMembers?.map((member) => (
+                            <SelectItem key={member.email} value={member.id}>
+                                {member.username}
+                            </SelectItem>
+                        ))}
                     </SelectContent>
                 </Select>
             </div>
